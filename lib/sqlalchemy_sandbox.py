07df17164db0064538044686edfac56442289623
AdamKelly <adam.obare@student.moringaschool.com>
2023-08-31 14:36:19 +0300

    Done

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from sqlalchemy import Column, Integer, String, create_engine
 
-from sqlalchemy import Column, Integer, String, 
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -12,7 +12,7 @@ class Student(Base):
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
-from sqalchemy import create_engine
+from sqlalchemy import create_engine
 if __name__ == '__main__':
     pass
     engine = create_engine('sqlite:///students.db')
